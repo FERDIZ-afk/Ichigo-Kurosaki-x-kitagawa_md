df1f5b68c1deb62120da1d8f2a4960961b6af6b1
FERDIZ-afk <ferdizakyla@gmail.com>
2022-10-26 00:45:01 +0700

    tes di vps

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/bash
 
-pkg install imagemagick git nodejs ffmpeg libwebp mc nano yarn
+#pkg install imagemagick git nodejs ffmpeg libwebp mc nano yarn
 rm -rf node_modules
 yarn
-rm -rf sesi.json
-npm start
+#rm -rf sesi.json
+
+cd node_modules/@adiwajshing/baileys && npm run build:tsc && cp src/Defaults/baileys-version.json lib/Defaults/baileys-version.json && cd && cd Ichigo-Kurosaki
+
+cp node_modules/@adiwajshing/baileys/src/Defaults/baileys-version.json node_modules/@adiwajshing/baileys/lib/Defaults/baileys-version.json
+ls node_modules/@adiwajshing/baileys/lib/Defaults
+
+
+npm run dev
+
 
 echo "All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
